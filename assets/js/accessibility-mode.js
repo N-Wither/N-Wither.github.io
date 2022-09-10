8d6cac4b35e2304b8e50ab732f99b8cdc92e1dfb
N-Wither <nwither2333@gmail.com>
2022-09-10 19:04:08 +0800

    Update 20220910-2
    bugfixes

--- assets/js/accessibility-mode.js
+++ assets/js/accessibility-mode.js
@@ -5,14 +5,14 @@ function accModeSwitch() {
     if (accessibilityModeIsOn == false) {
         body.setAttribute('class', 'accessibility-mode-on');
         navigate.setAttribute('class', 'navigate-accessibility');
-        document.getElementsByClassName('content').setAttribute('class', 'content-a11y');
+        document.getElementById('content').setAttribute('class', 'content-a11y');
         document.getElementById('toolbox').setAttribute('class', 'toolbox-a11y');
         accessibilityModeIsOn = true;
     }
     else {
         body.setAttribute('class', '');
         navigate.setAttribute('class', 'navigate');
-        document.getElementsByClassName('content-a11y').setAttribute('class', 'content');
+        document.getElementById('content-a11y').setAttribute('class', 'content');
         document.getElementById('toolbox').setAttribute('class', 'toolbox');
         accessibilityModeIsOn = false;
     }
