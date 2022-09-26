e50dd15ec56c68a3fc667f99ac1942ca29db870c
N-Wither <nwither2333@gmail.com>
2022-09-26 20:31:35 +0800

    Update 20220926-1

---
+++ assets/js/utils.js
@@ -0,0 +1,80 @@
+let darkModeIsOn = false;
+function darkModeSwitch() {
+    let body = document.body;
+    let navigate = document.getElementById('navigate');
+    if (darkModeIsOn == false) {
+        body.setAttribute('class', 'dark-mode-on');
+        navigate.setAttribute('class', 'navigate-dark');
+        document.getElementById('content').setAttribute('class', 'content-dark');
+        document.getElementById('toolbox').setAttribute('class', 'toolbox-dark');
+        darkModeIsOn = true;
+    }
+    else {
+        body.setAttribute('class', '');
+        navigate.setAttribute('class', 'navigate');
+        document.getElementById('content').setAttribute('class', 'content');
+        document.getElementById('toolbox').setAttribute('class', 'toolbox');
+        darkModeIsOn = false;
+    }
+}
+
+let accessibilityModeIsOn = false;
+function accModeSwitch() {
+    let body = document.body;
+    let navigate = document.getElementById('navigate');
+    if (accessibilityModeIsOn == false) {
+        body.setAttribute('class', 'accessibility-mode-on');
+        navigate.setAttribute('class', 'navigate-accessibility');
+        document.getElementById('content').setAttribute('class', 'content-a11y');
+        document.getElementById('toolbox').setAttribute('class', 'toolbox-a11y');
+        accessibilityModeIsOn = true;
+    }
+    else {
+        body.setAttribute('class', '');
+        navigate.setAttribute('class', 'navigate');
+        document.getElementById('content').setAttribute('class', 'content');
+        document.getElementById('toolbox').setAttribute('class', 'toolbox');
+        accessibilityModeIsOn = false;
+    }
+}
+
+function startTime(){
+    var today=new Date();
+    var D=today.getDate();
+    var Y=today.getFullYear();
+    var Mo=today.getMonth()+1;
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m=checkTime(m);
+    s=checkTime(s);
+    document.getElementById('clock').innerHTML=Y+"/"+ Mo +"/"+D+" "+h+":"+m+":"+s;
+    t=setTimeout(function(){startTime()},500);
+}
+function checkTime(i){
+    if (i<10){
+        i="0" + i;
+    }
+    return i;
+}
+
+function hello(){console.log("Wow, you found me!")}
+
+function randomQuote(){
+    let quotes = [
+        '已有的事后必再有，已行的事后必再行，日光之下并无新事。——《圣经·传道书》1:9',
+        '给岁月以文明，而不是给文明以岁月。——刘慈欣《三体·黑暗森林》',
+        '可以搞艺术，但是没必要。',
+        '如此生活三十年，直到大厦崩塌。——万能青年旅店《杀死那个石家庄人》',
+        '……可谁爱我呢？——老舍《茶馆》',
+        '还好，一切如愿以偿。——《红雪》',
+        '每逢你想要批评任何人的时候，要记住，这个世界上并非所有的人，都有你拥有的那些优势。——菲茨杰拉德《了不起的盖茨比》'
+    ];
+    let pick = getRandomInt(0, quotes.length);
+    document.getElementById('randomQuote').innerHTML = quotes[pick];
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
