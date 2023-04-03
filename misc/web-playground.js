ae23fcfb084bac96a99baf37b9d71415081cd04b
N-Wither <nwither2333@gmail.com>
2023-04-03 21:54:58 +0800

    update 230403-1

--- misc/web-playground.js
+++ misc/web-playground.js
@@ -5,6 +5,9 @@ let gameTimeMin = document.getElementById('moba-time-min')
 let gameTimeSec = document.getElementById('moba-time-sec')
 let gameTimeMinB = document.getElementById('moba-time-min-b')
 let gameTimeSecB = document.getElementById('moba-time-sec-b')
+let leagueLogoUrl = document.getElementById('league-logo-url')
+let leagueLogoImg = document.getElementById('league-logo-img')
+let leagueLogoFilter = true
 
 let teamBlueName = document.getElementById('moba-blue-team-name')
 let teamBlueNameText = document.getElementById('blue-team-name-text')
@@ -19,8 +22,6 @@ let teamBlueTurretBtnAdd = document.getElementById('moba-blue-tower-up')
 let teamBlueTurretBtnMin = document.getElementById('moba-blue-tower-down')
 let teamBlueGold = document.getElementById('blue-team-gold')
 let teamBlueGoldInput = document.getElementById('moba-blue-gold')
-let teamBlueGoldUp = document.getElementById('moba-blue-gold-up')
-let teamBlueGoldDn = document.getElementById('moba-blue-gold-down')
 let teamBlueKill = document.getElementById('blue-team-kill')
 let teamBlueKillInput = document.getElementById('moba-blue-kill')
 let teamBlueKillUp = document.getElementById('moba-blue-kill-up')
@@ -29,6 +30,9 @@ let teamBlueDragons = document.getElementById('team-blue-dragons')
 let teamBlueDragonsInput = document.getElementById('moba-blue-dragons')
 let teamBlueLogo = document.getElementById('blue-team-logo-url')
 let teamBlueWL = document.getElementById('team-blue-w-l')
+let teamBlueFilter = document.getElementById('blue-team-logo-filter')
+let teamBlueFilterOn = true
+let teamBlueColor = document.getElementById('team-blue-color')
 
 let teamRedName = document.getElementById('moba-red-team-name')
 let teamRedNameText = document.getElementById('red-team-name-text')
@@ -43,8 +47,6 @@ let teamRedTurretBtnAdd = document.getElementById('moba-red-tower-up')
 let teamRedTurretBtnMin = document.getElementById('moba-red-tower-down')
 let teamRedGold = document.getElementById('red-team-gold')
 let teamRedGoldInput = document.getElementById('moba-red-gold')
-let teamRedGoldUp = document.getElementById('moba-red-gold-up')
-let teamRedGoldDn = document.getElementById('moba-red-gold-down')
 let teamRedKill = document.getElementById('red-team-kill')
 let teamRedKillInput = document.getElementById('moba-red-kill')
 let teamRedKillUp = document.getElementById('moba-red-kill-up')
@@ -53,6 +55,42 @@ let teamRedDragons = document.getElementById('team-red-dragons')
 let teamRedDragonsInput = document.getElementById('moba-red-dragons')
 let teamRedLogo = document.getElementById('red-team-logo-url')
 let teamRedWL = document.getElementById('team-red-w-l')
+let teamRedFilter = document.getElementById('red-team-logo-filter')
+let teamRedFilterOn = true
+let teamRedColor = document.getElementById('team-red-color')
+
+let timerID; // 计时器 ID
+let timerCount = 0; // 计时器计数器
+let timerRunning = false; // 标记计时器是否正在运行
+
+// 获取按钮和 div 元素
+let timerButton = document.getElementById('moba-time-btn');
+
+// 定义按下按钮时的回调函数
+timerButton.onclick = function() {
+    if(!editBoard.checked) return;
+    if (timerRunning) {
+        // 如果计时器正在运行，则暂停计时器
+        clearInterval(timerID);
+        timerRunning = false;
+    } else {
+        // 否则启动计时器
+        timerID = setInterval(updateTimer, 1000);
+        timerRunning = true;
+    }
+}
+
+// 更新计时器计数器并更新 div 中的文本
+function updateTimer() {
+    timerCount = parseInt(gameTimeMin.value) * 60 + parseInt(gameTimeSec.value)
+    timerCount++;
+    let sec = timerCount % 60
+    let min = Math.floor(timerCount / 60)
+    gameTimeMin.value = min
+    gameTimeSec.value = sec
+    gameTimeMinB.innerHTML = formatNum(min)
+    gameTimeSecB.innerHTML = formatNum(sec)
+}
 
 editBoard.addEventListener('change', () => {
     refreshBoard()
@@ -71,6 +109,23 @@ function getBestOf(){
     return bo
 }
 
+function getScoreToWin(){
+    return Math.ceil(parseInt(getBestOf())/2)
+}
+
+function antiNegativeNum(num){
+    if(num <= 0){return 0}
+    else return num
+}
+
+function formatNum(num){
+    num = num || 0
+    if(num < 10){
+        return '0' + num
+    }
+    else return num
+}
+
 function setScore(num, team){
     let bestOf = getBestOf()
     let score = num
@@ -86,6 +141,7 @@ function setScore(num, team){
 }
 
 function setDragon(dragons, team){
+    if(!editBoard.checked) return;
     let HTML = ''
     let dragonList = dragons.trim().split(' ')
     if(dragons === ''){
@@ -99,11 +155,53 @@ function setDragon(dragons, team){
 }
 
 function addDragon(team, dragon){
+    if(!editBoard.checked) return
     if(team == 'blue'){
         teamBlueDragonsInput.value = teamBlueDragonsInput.value + dragon + ' '
     }
     if(team == 'red'){
         teamRedDragonsInput.value = teamRedDragonsInput.value + dragon + ' '
+    }
+}
+
+function setNumCap(target, min, max){
+    target.min = min
+    target.max = max
+}
+
+function clearGameTime(){
+    gameTimeMin.value = 0
+    gameTimeMinB.innerHTML = '00'
+    gameTimeSec.value = 0
+    gameTimeSecB.innerHTML = '00'
+    timerCount = 0
+}
+
+function addGold(team, gold){
+    if(!editBoard.checked) return;
+    if(team == 'blue'){
+        teamBlueGoldInput.value = parseFloat((parseFloat(teamBlueGoldInput.value) + gold).toFixed(1))
+        if(teamBlueGoldInput.value <= 0){
+            teamBlueGoldInput.value = '0.0'
+        }
+        teamBlueGold.innerHTML = parseFloat(teamBlueGoldInput.value).toFixed(1) + 'K'
+    }
+    if(team == 'red'){
+        teamRedGoldInput.value = parseFloat((parseFloat(teamRedGoldInput.value) + gold).toFixed(1))
+        if(teamRedGoldInput.value <= 0){
+            teamRedGoldInput.value = '0.0'
+        }
+        teamRedGold.innerHTML = parseFloat(teamRedGoldInput.value).toFixed(1) + 'K'
+    }
+}
+
+document.getElementById('league-logo-filter-btn').onclick = () => {
+    if(leagueLogoFilter){
+        document.getElementById('league-logo-img').style.filter = 'none'
+        leagueLogoFilter = false
+    }else{
+        document.getElementById('league-logo-img').style.filter = 'brightness(0) invert()'
+        leagueLogoFilter = true
     }
 }
 
@@ -114,19 +212,39 @@ function refreshBoard(){
     })
     if(!editBoard.checked){return}
 
-    gameTimeMin.addEventListener('input', () => {
-        if(gameTimeMin.value < 10){
-            gameTimeMinB.innerHTML = '0' + gameTimeMin.value
-        }else{
-            gameTimeMinB.innerHTML = gameTimeMin.value
+    gameTimeMin.addEventListener(('change', 'input'), () => {
+        if(gameTimeMin.value == ''){
+            gameTimeMin.value = 0
         }
+        gameTimeMinB.innerHTML = '' + formatNum(parseInt(gameTimeMin.value))
     })
-    gameTimeSec.addEventListener('input', () => {
+    gameTimeSec.addEventListener(('change', 'input'), () => {
+        if(gameTimeSec.value == ''){
+            gameTimeSec.value = 0
+        }
+        if(gameTimeSec.value == 60){
+            gameTimeSec.value = 0
+            gameTimeMin.value++
+            gameTimeMinB.innerHTML = formatNum(gameTimeMin.value)
+        }
+        if(gameTimeSec.value == -1){
+            if(gameTimeMin.value == 0){
+                gameTimeSec.value = 0
+                return
+            };
+            gameTimeSec.value = 59
+            gameTimeMin.value--
+            gameTimeMinB.innerHTML = formatNum(gameTimeMin.value)
+        }
         if(parseInt(gameTimeSec.value) < 10){
-            gameTimeSecB.innerHTML = '0' + gameTimeSec.value
+            gameTimeSecB.innerHTML = '0' + parseInt(gameTimeSec.value)
         }else{
             gameTimeSecB.innerHTML = gameTimeSec.value
         }
+    })
+
+    leagueLogoUrl.addEventListener(('change', 'input'), () => {
+        leagueLogoImg.src = leagueLogoUrl.value
     })
 
     // All about blue team
@@ -136,62 +254,82 @@ function refreshBoard(){
     teamBluePos.addEventListener('input', () => {
         teamBluePosText.innerHTML = teamBluePos.value
     })
-    teamBlueScore.addEventListener('change', () => {
+    teamBlueScore.addEventListener(('change', 'input'), () => {
         let score = teamBlueScore.value
+        setNumCap(teamBlueScore, 0, getScoreToWin())
         setScore(score, 'blue')
     })
     teamBlueScoreBtnAdd.addEventListener('click', () => {
+        if(teamBlueScore.value >= getScoreToWin()){return}
+        setNumCap(teamBlueScore, 0, getScoreToWin())
         teamBlueScore.value++
         setScore(teamBlueScore.value, 'blue')
     })
     teamBlueScoreBtnMin.addEventListener('click', () => {
+        if(teamBlueScore.value <= 0){return}
+        setNumCap(teamBlueScore, 0, getScoreToWin())
         teamBlueScore.value--
         setScore(teamBlueScore.value, 'blue')
     })
-    teamBlueTurretInput.addEventListener('change', () => {
-        teamBlueTurret.innerHTML = teamBlueTurretInput.value
+    teamBlueTurretInput.addEventListener(('change', 'input'), () => {
+        teamBlueTurretInput.value = antiNegativeNum(teamBlueTurretInput.value)
+        teamBlueTurret.innerHTML = antiNegativeNum(teamBlueTurretInput.value)
     })
     teamBlueTurretBtnAdd.addEventListener('click', () => {
         teamBlueTurretInput.value ++
+        teamBlueTurretInput.value = antiNegativeNum(teamBlueTurretInput.value)
         teamBlueTurret.innerHTML = teamBlueTurretInput.value
     })
     teamBlueTurretBtnMin.addEventListener('click', () => {
         teamBlueTurretInput.value --
-        teamBlueTurret.innerHTML = teamBlueTurretInput.value
+        teamBlueTurretInput.value = antiNegativeNum(teamBlueTurretInput.value)
+        teamBlueTurret.innerHTML = antiNegativeNum(teamBlueTurretInput.value)
     })
-    teamBlueGoldInput.addEventListener('change', () => {
+    teamBlueGoldInput.addEventListener(('change', 'input'), () => {
         teamBlueGold.innerHTML = parseFloat(teamBlueGoldInput.value).toFixed(1) + 'K'
     })
-    teamBlueGoldUp.addEventListener('click', () => {
-        teamBlueGoldInput.value = (parseFloat(teamBlueGoldInput.value) + 0.1).toFixed(1)
-        teamBlueGold.innerHTML = teamBlueGoldInput.value + 'K'
-    })
-    teamBlueGoldDn.addEventListener('click', () => {
-        teamBlueGoldInput.value = (parseFloat(teamBlueGoldInput.value) - 0.1).toFixed(1)
-        teamBlueGold.innerHTML = teamBlueGoldInput.value + 'K'
-    })
-    teamBlueKillInput.addEventListener('change', () => {
+    teamBlueKillInput.addEventListener(('change', 'input'), () => {
+        teamBlueKillInput.value = antiNegativeNum(teamBlueKillInput.value)
         teamBlueKill.innerHTML = teamBlueKillInput.value
     })
     teamBlueKillUp.addEventListener('click', () => {
         teamBlueKillInput.value ++
+        teamBlueKillInput.value = antiNegativeNum(teamBlueKillInput.value)
         teamBlueKill.innerHTML = teamBlueKillInput.value
     })
     teamBlueKillDn.addEventListener('click', () => {
         teamBlueKillInput.value --
+        teamBlueKillInput.value = antiNegativeNum(teamBlueKillInput.value)
         teamBlueKill.innerHTML = teamBlueKillInput.value
     })
-    teamBlueDragonsInput.addEventListener('change', () => {
+    teamBlueDragonsInput.addEventListener(('change', 'input'), () => {
         let dragons = teamBlueDragonsInput.value
         setDragon(dragons, 'blue')
     })
-    teamBlueLogo.addEventListener('change', () => {
-        let url = teamBlueLogo.value
-        document.getElementById('blue-team-logo').src = url
-        document.getElementById('blue-team-logo').alt = teamBlueName.value
+    teamBlueFilter.addEventListener('change', () => {
+        if(teamBlueFilterOn){
+            teamBlueFilterOn = false
+        }
+        else teamBlueFilterOn = true
+        if(!teamBlueFilterOn){
+            document.getElementById('blue-team-logo').style.filter = 'none'
+        }
+        else{
+            document.getElementById('blue-team-logo').style.filter = 'brightness(0) invert()'
+        }
     })
-    teamBlueWL.addEventListener('input', () => {
+    teamBlueLogo.addEventListener(('change', 'input'), () => {
+        let url = teamBlueLogo.value
+        let logo = document.getElementById('blue-team-logo')
+        logo.src = url
+        logo.alt = teamBlueName.value
+    })
+    teamBlueWL.addEventListener(('change', 'input'), () => {
         document.getElementById(`team-blue-score`).innerHTML = teamBlueWL.value
+    })
+    teamBlueColor.addEventListener(('input', 'change'), () => {
+        let color = teamBlueColor.value
+        document.getElementById('team-blue-logo-bg').style.backgroundColor = color
     })
 
     //All about red team
@@ -201,61 +339,79 @@ function refreshBoard(){
     teamRedPos.addEventListener('input', () => {
         teamRedPosText.innerHTML = teamRedPos.value
     })
-    teamRedScore.addEventListener('change', () => {
+    teamRedScore.addEventListener(('change', 'input'), () => {
+        setNumCap(teamRedScore, 0, getScoreToWin())
         let score = teamRedScore.value
         setScore(score, 'red')
     })
     teamRedScoreBtnAdd.addEventListener('click', () => {
+        if(teamRedScore.value >= getScoreToWin()){return}
+        // setNumCap(teamRedScore, 0, getScoreToWin())
         teamRedScore.value++
         setScore(teamRedScore.value, 'red')
     })
     teamRedScoreBtnMin.addEventListener('click', () => {
+        if(teamRedScore.value <= 0){return}
+        // (teamRedScore, 0, getScoreToWin())
         teamRedScore.value--
         setScore(teamRedScore.value, 'red')
     })
-    teamRedTurretInput.addEventListener('change', () => {
+    teamRedTurretInput.addEventListener(('change', 'input'), () => {
+        teamRedTurretInput.value = antiNegativeNum(teamRedTurretInput.value)
         teamRedTurret.innerHTML = teamRedTurretInput.value
     })
     teamRedTurretBtnAdd.addEventListener('click', () => {
         teamRedTurretInput.value ++
+        teamRedTurretInput.value = antiNegativeNum(teamRedTurretInput.value)
         teamRedTurret.innerHTML = teamRedTurretInput.value
     })
     teamRedTurretBtnMin.addEventListener('click', () => {
         teamRedTurretInput.value --
+        teamRedTurretInput.value = antiNegativeNum(teamRedTurretInput.value)
         teamRedTurret.innerHTML = teamRedTurretInput.value
     })
-    teamRedGoldInput.addEventListener('change', () => {
+    teamRedGoldInput.addEventListener(('change', 'input'), () => {
         teamRedGold.innerHTML = parseFloat(teamRedGoldInput.value).toFixed(1) + 'K'
     })
-    teamRedGoldUp.addEventListener('click', () => {
-        teamRedGoldInput.value = (parseFloat(teamRedGoldInput.value) + 0.1).toFixed(1)
-        teamRedGold.innerHTML = teamRedGoldInput.value + 'K'
-    })
-    teamRedGoldDn.addEventListener('click', () => {
-        teamRedGoldInput.value = (parseFloat(teamRedGoldInput.value) - 0.1).toFixed(1)
-        teamRedGold.innerHTML = teamRedGoldInput.value + 'K'
-    })
-    teamRedKillInput.addEventListener('change', () => {
-        teamRedKill.innerHTML = teamRedKillInput.value
+    teamRedKillInput.addEventListener(('change', 'input'), () => {
+        teamRedKill.innerHTML = antiNegativeNum(teamRedKillInput.value)
     })
     teamRedKillUp.addEventListener('click', () => {
         teamRedKillInput.value ++
+        teamRedKillInput.value = antiNegativeNum(teamRedKillInput.value)
         teamRedKill.innerHTML = teamRedKillInput.value
     })
     teamRedKillDn.addEventListener('click', () => {
         teamRedKillInput.value --
+        teamRedKillInput.value = antiNegativeNum(teamRedKillInput.value)
         teamRedKill.innerHTML = teamRedKillInput.value
     })
-    teamRedDragonsInput.addEventListener('change', () => {
+    teamRedDragonsInput.addEventListener(('change', 'input'), () => {
         let dragons = teamRedDragonsInput.value
         setDragon(dragons, 'red')
     })
-    teamRedLogo.addEventListener('change', () => {
+    teamRedFilter.addEventListener('change', () => {
+        if(teamRedFilterOn){
+            teamRedFilterOn = false
+        }
+        else teamRedFilterOn = true
+        if(!teamRedFilterOn){
+            document.getElementById('red-team-logo').style.filter = 'none'
+        }
+        else{
+            document.getElementById('red-team-logo').style.filter = 'brightness(0) invert()'
+        }
+    })
+    teamRedLogo.addEventListener(('change', 'input'), () => {
         let url = teamRedLogo.value
         document.getElementById('red-team-logo').src = url
         document.getElementById('red-team-logo').alt = teamRedName.value
     })
     teamRedWL.addEventListener('input', () => {
         document.getElementById(`team-red-score`).innerHTML = teamRedWL.value
+    })
+    teamRedColor.addEventListener(('input', 'change'), () => {
+        let color = teamRedColor.value
+        document.getElementById('team-red-logo-bg').style.backgroundColor = color
     })
 }
