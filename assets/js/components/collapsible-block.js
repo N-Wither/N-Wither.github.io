0577cc37b74fb42052e11fb0973f7138997318c5
N-Wither <nwither2333@gmail.com>
2023-07-04 11:22:10 +0800

    update 230704-1

--- assets/js/components/collapsible-block.js
+++ assets/js/components/collapsible-block.js
@@ -8,42 +8,52 @@
     </div>
 */
 
+let blocks = document.getElementsByClassName('collapsible')
 
-document.querySelectorAll('.collapsible').forEach(el => {
+for(let i = 0; i < blocks.length; i ++){
+    let el = blocks[i]
+
     let openLinkText = el.querySelector('.open-link').innerHTML || 'Show'
     let closeLinkText = el.querySelector('.close-link').innerHTML || 'Hide'
     let content = el.querySelector('.content').innerHTML
-    let template = 
-    `<div class="collapsible__link-container">
-        <a href="javascript:;" class="collapsible__link">${openLinkText}</a>
-    </div>
-    <div class="collapsible__content">${content}</div>`
+    let template =
+        `<div class="collapsible__link-container">
+            <button class="collapsible__link">${openLinkText}</button>
+        </div>
+        <div class="collapsible__content">${content}</div>`
 
     el.innerHTML = template
 
     let computedContent = el.querySelector('.collapsible__content')
-    let height = getComputedStyle(computedContent).height
-    computedContent.style.height = 0
 
     let link = el.querySelector('.collapsible__link')
 
-    window.addEventListener('resize', () => {
-        computedContent.style.height = 'auto'
-        height = getComputedStyle(computedContent).height
-        if(link.innerHTML == openLinkText){
-            computedContent.style.height = 0
-        }else {
-            computedContent.style.height = height
-        }
-    })
-    
     link.addEventListener('click', () => {
-        if(link.innerHTML == openLinkText){
+        if (link.innerHTML == openLinkText) {
             link.innerHTML = closeLinkText
-            computedContent.style.height = height
-        }else{
+            link.classList.toggle('open')
+            computedContent.style.maxHeight = computedContent.scrollHeight + 'px'
+        } else {
             link.innerHTML = openLinkText
-            computedContent.style.height = 0
+            link.classList.toggle('open')
+            computedContent.style.maxHeight = 0
         }
     })
-})
+
+    computedContent.addEventListener('click', () => {
+        computedContent.style.maxHeight = 'fit-content'
+        setTimeout(() => {
+            computedContent.style.maxHeight = computedContent.scrollHeight + 'px'
+        }, 500)
+    })
+
+    window.addEventListener('resize', () => {
+        computedContent.style.maxHeight = 'fit-content'
+        setTimeout(() => {
+            computedContent.style.maxHeight = computedContent.scrollHeight + 'px'
+        }, 500)
+    })
+
+    computedContent.style.maxHeight = 0
+
+}
