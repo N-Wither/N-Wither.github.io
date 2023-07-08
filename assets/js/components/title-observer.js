02fab82c9653cf4df5c89446b4587b6a85a4780a
N-Wither <nwither2333@gmail.com>
2023-07-08 14:09:04 +0800

    update 230708-1

--- assets/js/components/title-observer.js
+++ assets/js/components/title-observer.js
@@ -1,10 +1,8 @@
 let titleObserver = new IntersectionObserver(entries => {
     if(entries[0].intersectionRatio <= 0){
         document.querySelector('.page-header .page-title').style.opacity = 1
-        console.log('leave')
     }else{
         document.querySelector('.page-header .page-title').style.opacity = 0
-        console.log('enter')
     }
 })
 
