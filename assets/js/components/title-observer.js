9692c8b9a490114a2656ce1d12fff7bc21e04f73
N-Wither <nwither2333@gmail.com>
2023-07-18 18:56:32 +0800

    update 230718-3

--- assets/js/components/title-observer.js
+++ assets/js/components/title-observer.js
@@ -7,20 +7,23 @@ toTop.id = 'to-top'
 toTop.style.position = 'fixed'
 toTop.style.right = '20px'
 toTop.style.bottom = '20px'
+toTop.style.zIndex = '99'
 toTopButton.classList.add('menu-button')
 toTopButton.addEventListener('click', () => {
     window.scrollTo(0, 0)
 })
+document.body.appendChild(toTop)
+toTop.style.display = 'none'
 
 let titleObserver = new IntersectionObserver(entries => {
     if(entries[0].intersectionRatio <= 0){
         document.querySelector('.page-header .page-title').style.opacity = 1
-        document.body.appendChild(toTop)
+        toTop.style.display = 'block'
         toTop.classList.remove('ani__hide')
     }else{
         document.querySelector('.page-header .page-title').style.opacity = 0
         toTop.classList.add('ani__hide')
-        setTimeout(() => {document.body.removeChild(toTop)}, 400)
+        setTimeout(() => {toTop.style.display = 'none'}, 400)
     }
 })
 
