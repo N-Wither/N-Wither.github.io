91f2990d7bc35ae34103ec66f1985693fa13d023
N-Wither <nwither2333@gmail.com>
2023-09-07 21:36:16 +0800

    update 230907-5

--- assets/js/components/hover-tooltip.js
+++ assets/js/components/hover-tooltip.js
@@ -7,18 +7,21 @@ if(needTooltip.length == 0) console.warn('HoverTooltip: No tooltip is needed!')
 if(tooltipContents.length == 0) console.warn('HoverTooltip: No tooltip content found!')
 
 let tippyConfig = {
-    placement: 'right',
+    placement: 'auto',
     allowHTML: true,
     interactive: true,
     theme: 'aquamarine',
-    arrow: false
+    arrow: false,
+    maxWidth: '70vw',
+    // hideOnClick: false,
+    // trigger: 'click'
 }
 
 needTooltip.forEach((el, i) => {
     if(needTooltip.length == 0 || tooltipContents.length == 0) return;
     let index = el.getAttribute('tooltip-index') || (i + 1)
-    let content = document.querySelector(`#tooltip-content-${index}`)
+    let content = document.querySelector(`#tooltip-content-${index}`) || 'Error: content not found.'
     let tooltip = tippy(el, {content: content.innerHTML, ...tippyConfig})
     tooltip.popper.firstChild.dataset.index = el.textContent
-    console.log(tooltip.popper)
+    // console.log(tooltip.popper)
 })
