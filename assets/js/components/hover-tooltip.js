cf58bc4b420eee008b87bf32bc765e45c33ffc20
N-Wither <nwither2333@gmail.com>
2023-09-07 20:34:23 +0800

    update 230907-2

--- assets/js/components/hover-tooltip.js
+++ assets/js/components/hover-tooltip.js
@@ -1,76 +1,24 @@
-// First import Popper.js from here: https://unpkg.com/@popperjs/core@2
+// First import Popper.js from here: https://unpkg.com/@popperjs/core@2 and https://unpkg.com/tippy.js@6
 
 let needTooltip = document.querySelectorAll('.need-tooltip')
 let tooltipContents = document.querySelectorAll('.tooltip-content')
 
-let popperConfig = {
-    placement: 'right',
-    modifiers: [
-        {
-            name: 'offset',
-            options: {offset: [0, 12]}
-        }
-    ]
-}
-
 if(needTooltip.length == 0) console.warn('HoverTooltip: No tooltip is needed!')
 if(tooltipContents.length == 0) console.warn('HoverTooltip: No tooltip content found!')
 
+let tippyConfig = {
+    placement: 'right',
+    allowHTML: true,
+    interactive: true,
+    theme: 'aquamarine',
+    arrow: false
+}
+
 needTooltip.forEach((el, i) => {
     if(needTooltip.length == 0 || tooltipContents.length == 0) return;
-    let tooltip = document.createElement('div')
     let index = el.getAttribute('tooltip-index') || (i + 1)
     let content = document.querySelector(`#tooltip-content-${index}`)
-    
-    tooltip.classList.add('tooltip')
-    tooltip.id = `tooltip-${index}`
-    tooltip.innerHTML = content.innerHTML
-    tooltip.dataset.index = el.textContent
-
-    el.appendChild(tooltip)
-
-    let popperInstance = Popper.createPopper(el, document.getElementById(tooltip.id), popperConfig)
-
-    function show() {
-        // Make the tooltip visible
-        tooltip.setAttribute('data-show', '');
-
-        // Enable the event listeners
-        popperInstance.setOptions((options) => ({
-            ...options,
-            modifiers: [
-                ...options.modifiers,
-                { name: 'eventListeners', enabled: true },
-            ],
-        }));
-
-        // Update its position
-        popperInstance.update();
-    }
-
-    function hide() {
-        // Hide the tooltip
-        tooltip.removeAttribute('data-show');
-
-        // Disable the event listeners
-        popperInstance.setOptions((options) => ({
-            ...options,
-            modifiers: [
-                ...options.modifiers,
-                { name: 'eventListeners', enabled: false },
-            ],
-        }));
-    }
-
-    const showEvents = ['mouseenter', 'focus'];
-    const hideEvents = ['mouseleave', 'blur'];
-
-    showEvents.forEach((event) => {
-        el.addEventListener(event, show);
-    });
-
-    hideEvents.forEach((event) => {
-        el.addEventListener(event, hide);
-    });
-
+    let tooltip = tippy(el, {content: content.innerHTML, ...tippyConfig})
+    console.log(tooltip.popper)
+    tooltip.popper.firstChild.dataset.index = el.textContent
 })
