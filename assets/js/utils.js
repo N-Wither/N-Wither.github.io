7976cd79860f8500e7fa348918c0f3db9b9a4b13
N-Wither <nwither2333@gmail.com>
2023-09-10 09:18:29 +0800

    update 230910-1

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -6,23 +6,12 @@ if(matchScheme.matches && !document.querySelector('html').classList.contains('da
 
 // Clock component
 function startTime() {
-    var today = new Date();
-    var D = today.getDate();
-    var Y = today.getFullYear();
-    var Mo = today.getMonth() + 1;
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = Y + "/" + Mo + "/" + D + " " + h + ":" + m + ":" + s;
-    t = setTimeout(function () { startTime() }, 500);
-}
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    document.getElementById('clock').innerHTML = `${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()} ${h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`
+    let t = setTimeout(() => startTime() , 500);
 }
 
 // A useless func
