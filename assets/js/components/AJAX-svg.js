061e447ff347bdf6fbbf875aa63d51382abc174a
N-Wither <nwither2333@gmail.com>
2023-09-28 16:12:35 +0800

    update 230928-2

--- assets/js/components/AJAX-svg.js
+++ assets/js/components/AJAX-svg.js
@@ -1,9 +1,13 @@
-let thingsNeedSvg = document.querySelectorAll('.request-svg')
-if(thingsNeedSvg){
-    thingsNeedSvg.forEach(async el => {
-        el.innerHTML = await betterFetch(el.dataset.src)
-        if(el.classList.length == 1) el.removeAttribute('class');
-        else el.classList.remove('request-svg');
-        el.removeAttribute('data-src')
-    })
+let ajaxSvg = () => {
+    let thingsNeedSvg = document.querySelectorAll('.request-svg')
+    if(thingsNeedSvg){
+        thingsNeedSvg.forEach(async el => {
+            el.innerHTML = await betterFetch(el.dataset.src)
+            if(el.classList.length == 1) el.removeAttribute('class');
+            else el.classList.remove('request-svg');
+            el.removeAttribute('data-src')
+        })
+    }
 }
+
+ajaxSvg()
