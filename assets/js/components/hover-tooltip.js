4b4236b6589dedba3a8be3a44c09101a8dda98ae
N-Wither <nwither2333@gmail.com>
2023-10-03 17:28:05 +0800

    update 231003-1

--- assets/js/components/hover-tooltip.js
+++ assets/js/components/hover-tooltip.js
@@ -1,12 +1,6 @@
 // First import Popper.js from here: https://unpkg.com/@popperjs/core@2
 // Then import Tippy.js from here https://unpkg.com/tippy.js@6
 
-let needTooltip = document.querySelectorAll('.need-tooltip')
-let tooltipContents = document.querySelectorAll('.tooltip-content')
-
-if(needTooltip.length == 0) console.warn('HoverTooltip: No tooltip is needed!')
-if(tooltipContents.length == 0) console.warn('HoverTooltip: No tooltip content found!')
-
 let tippyConfig = {
     placement: 'auto',
     allowHTML: true,
@@ -18,19 +12,29 @@ let tippyConfig = {
     // trigger: 'click'
 }
 
-needTooltip.forEach((el, i) => {
-    if(needTooltip.length == 0 || tooltipContents.length == 0) return;
-    let error = document.createElement('div')
-    let index = el.dataset.tooltipIndex || (i + 1)
-    let replacePopperIndex = false
-    if(typeof index == 'string') {
-        index = index.replace(' ', '_')
-        replacePopperIndex = true
-    }
-    error.innerHTML = `Error: content (${index}) not found.`
-    let content = document.querySelector(`#tooltip-content-${index}`) || error
-    let tooltip = tippy(el, {content: content.innerHTML, ...tippyConfig})
-    if(!replacePopperIndex) tooltip.popper.firstChild.dataset.index = el.textContent;
-    else tooltip.popper.firstChild.dataset.index = index.replace('_', ' ')
-    // console.log(tooltip.popper)
-})
+function setTooltip() {
+    let needTooltip = document.querySelectorAll('.need-tooltip')
+    let tooltipContents = document.querySelectorAll('.tooltip-content')
+
+    if (needTooltip.length == 0) console.warn('HoverTooltip: No tooltip is needed!')
+    if (tooltipContents.length == 0) console.warn('HoverTooltip: No tooltip content found!')
+
+    needTooltip.forEach((el, i) => {
+        if (needTooltip.length == 0 || tooltipContents.length == 0) return;
+        let error = document.createElement('div')
+        let index = el.dataset.tooltipIndex || (i + 1)
+        let replacePopperIndex = false
+        if (typeof index == 'string') {
+            index = index.replace(' ', '_')
+            replacePopperIndex = true
+        }
+        error.innerHTML = `Error: content (${index}) not found.`
+        let content = document.querySelector(`#tooltip-content-${index}`) || error
+        let tooltip = tippy(el, { content: content.innerHTML, ...tippyConfig })
+        if (!replacePopperIndex) tooltip.popper.firstChild.dataset.index = el.textContent;
+        else tooltip.popper.firstChild.dataset.index = index.replace('_', ' ')
+        // console.log(tooltip.popper)
+    })
+}
+
+setTooltip()
