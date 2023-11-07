f4a3c60e56ca721f4247ed4e42e4b59943e5a7ba
N-Wither <nwither2333@gmail.com>
2023-11-07 16:12:35 +0800

    update 231107-2

--- web-components/aquamarine/card/card.js
+++ web-components/aquamarine/card/card.js
@@ -1,18 +1,20 @@
 const style = await (await fetch('/web-components/aquamarine/card/card.css')).text()
 const styleSheet = new CSSStyleSheet()
 styleSheet.replaceSync(style)
-document.adoptedStyleSheets = document.adoptedStyleSheets.concat(styleSheet)
 
 const template = 
 `
-<div part='card-image-container'>
-    <slot name='image' part='image'></slot>
+<div class='card__base'>
+    <div part='card-image-container' class='card__image-container'>
+        <slot name='image' part='image' class='card__image'></slot>
+    </div>
+    <div part='card-content' class='card__content'>
+        <slot name='header' part='card-header' class='card__header'></slot>
+        <slot part='card-body' class='card__body'></slot>
+        <slot name='footer' part='card-footer' class='card__footer'></slot>
+    </div>
 </div>
-<div part='card-content'>
-    <slot name='header' part='card-header'></slot>
-    <slot part='card-body'></slot>
-    <slot name='footer' part='card-footer'></slot>
-</div>
+
 `
 
 export class AqCard extends HTMLElement {
@@ -20,6 +22,20 @@ export class AqCard extends HTMLElement {
         super()
         this.attachShadow({mode: 'open'})
         this.shadowRoot.innerHTML = template
+        this.shadowRoot.adoptedStyleSheets.push(styleSheet)
+    }
+
+    connectedCallback() {
+        let horizontal = this.getAttribute('horizontal')
+        let base = this.shadowRoot.querySelector('.card__base')
+        if(horizontal != null){
+            base.classList.add('horizontal')
+        }
+        this.childNodes.forEach(el => {
+            if(el.slot == 'image') base.classList.add('has-image')
+            if(el.slot == 'header') base.classList.add('has-header')
+            if(el.slot == 'footer') base.classList.add('has-footer')
+        })
     }
 }
 
