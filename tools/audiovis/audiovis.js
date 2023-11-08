534dd8efb186991207069d1dbbee256b3e8d3aeb
N-Wither <nwither2333@gmail.com>
2023-11-08 10:56:35 +0800

    update 231108-1

--- tools/audiovis/audiovis.js
+++ tools/audiovis/audiovis.js
@@ -2,7 +2,8 @@ let file = document.querySelector('input')
 let canvas = document.querySelector('canvas')
 let audio = document.querySelector('audio')
 
-canvas.width = document.querySelector('.page-content').scrollWidth
+canvas.width = document.querySelector('.page-content').scrollWidth * devicePixelRatio
+canvas.height = 256 * devicePixelRatio
 
 let canvasCtx = canvas.getContext('2d')
 let audioCtx = new AudioContext()
@@ -12,17 +13,17 @@ let analyser = audioCtx.createAnalyser()
 
 source.connect(analyser)
 analyser.connect(audioCtx.destination)
-analyser.fftSize = 256
+analyser.fftSize = 128
 
 let buffer = new Uint8Array(analyser.frequencyBinCount)
 
 let draw = () => {
-    let width = canvas.scrollWidth / 128
+    let width = canvas.width / 64
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
     canvasCtx.fillStyle = '#20d6b5'
     let x = 0
     for(let i = 0 ; i < buffer.length; i ++) {
-        let height = buffer[i]
+        let height = buffer[i] * devicePixelRatio
         canvasCtx.fillRect(x, canvas.height - height, width, height)
 
         x += width
