ab6213137fbbc217c48609ff5def2c5d48963cd3
N-Wither <nwither2333@gmail.com>
2023-11-30 21:27:03 +0800

    update 231130-1

--- assets/js/components/title-observer.js
+++ assets/js/components/title-observer.js
@@ -1,13 +1,15 @@
 let toTop = document.createElement('div')
 let toTopButton = document.createElement('button')
-toTopButton.innerHTML = '▲'
+toTopButton.innerHTML = '\ue25a'
 toTop.appendChild(toTopButton)
+toTop.title = 'To Top'
 toTop.classList.add('float-div')
 toTop.id = 'to-top'
 toTop.style.position = 'fixed'
 toTop.style.right = '20px'
 toTop.style.bottom = '20px'
 toTop.style.zIndex = '99'
+toTopButton.style.fontSize = '1.8em'
 toTopButton.classList.add('menu-button')
 toTopButton.addEventListener('click', () => {
     window.scrollTo(0, 0)
