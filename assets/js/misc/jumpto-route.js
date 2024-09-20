42f46b975fa19c417a347edf4872c3c6b72a6787
N-Wither <nwither2333@gmail.com>
2024-09-21 00:34:32 +0800

    update 240921-2 jumpto.js is now synchronous to optimize load time.

--- assets/js/misc/jumpto-route.js
+++
@@ -1,5 +0,0 @@
-export default {
-    'meteor': '/projects/c/stories/starry/when-meteors-meet.html',
-    'c': '/projects/c/',
-    'omni': '/projects/omniworld/',
-}
--- assets/js/misc/jumpto.js
+++ assets/js/misc/jumpto.js
@@ -1,18 +1,20 @@
-if(window.location.search) {
-    let params = window.location.search.substring(1).split("&");
-    let result = {};
-    params.forEach(function(param) {
-        let keyValue = param.split("=");
-        result[keyValue[0]] = keyValue[1];
-    });
-    if('jumpto' in result) {
-        let route = (await import('./jumpto-route.js')).default
-        let link = route[result['jumpto']]
-        if(link) {
-            window.location.href = link
-        }
-        else {
-            window.location.href = '/404.html'
+(async () => {
+    if(window.location.search) {
+        let params = window.location.search.substring(1).split("&");
+        let result = {};
+        params.forEach(function(param) {
+            let keyValue = param.split("=");
+            result[keyValue[0]] = keyValue[1];
+        });
+        if('jumpto' in result) {
+            let route = await fetch('/assets/js/misc/jumpto-route.json').then(r => r.json())
+            let link = route[result['jumpto']]
+            if(link) {
+                window.location.href = link
+            }
+            else {
+                window.location.href = '/404.html'
+            }
         }
     }
-}
+})()
