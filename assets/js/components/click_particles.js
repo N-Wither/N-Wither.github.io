45c5d90479e81f5a9f67d751aa5e63934c87ebcf
NWitherKawaii <nwither2333@gmail.com>
2025-10-26 14:08:28 +0800

    update 2025-10-26/1

--- assets/js/components/click_particles.js
+++ assets/js/components/click_particles.js
@@ -1,15 +1,23 @@
 let clickParticleConfig = {
-    // the content of particle elements
-    // Array is supported
+    /**
+     * @type {string | string[]}
+     * the content of particle elements, Array is supported
+     */
     content: '★',
-    // the color of particles
-    // Valid values: 'random' | hex color code
-    color: 'random'
+    /**
+     * @type {string}
+     * the color of particles, Valid values: 'random' | hex color code
+     */
+    color: 'random',
+    /**
+     * @type {'ascent' | 'parabola'}
+     */
+    animation: 'ascent'
 }
 
 window.addEventListener('click', e => {
     let newParticle = document.createElement('div')
-    newParticle.classList.add('click-particle')
+    newParticle.classList.add('click-particle', clickParticleConfig.animation)
     newParticle.style.top = `calc(${e.clientY}px - 1rem)`
     newParticle.style.left = e.clientX + 'px'
     if(clickParticleConfig.color != 'random'){
@@ -23,9 +31,22 @@ window.addEventListener('click', e => {
         newParticle.innerHTML = clickParticleConfig.content
     }
     else{
-        newParticle.innerHTML = clickParticleConfig.content.getRandom()
+        newParticle.innerHTML = clickParticleConfig.content[Math.floor(Math.random() * clickParticleConfig.content.length)]
     }
     document.body.appendChild(newParticle)
+    if(clickParticleConfig.animation == 'parabola'){
+        let speedX = Math.random() * 2 - 1
+        let speedY = -Math.random() * 2 - 1
+        let particleInterval = setInterval(() => {
+            newParticle.style.left = parseFloat(newParticle.style.left) + speedX + 'px'
+            newParticle.style.top = parseFloat(newParticle.style.top) + speedY + 'px'
+            speedY += 0.1
+            if(parseFloat(newParticle.style.top) > window.innerHeight){
+                clearInterval(particleInterval)
+                newParticle.remove()
+            }
+        }, 1000 / 60)
+    }
     newParticle.addEventListener('animationend', () => {
         newParticle.remove()
     })
